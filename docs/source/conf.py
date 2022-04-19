20ec5ecd9c30555a915810772265a7c443415624
robinaguilar <eaguil@uw.edu>
2022-04-18 19:20:57 -0700

    added new setup and yml

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,9 +10,17 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+import subprocess
+
+
+sys.path.insert(0, os.path.abspath(os.path.join("..", "..")))
+
+try:
+    import numpydoc
+except ModuleNotFoundError:
+    subprocess.run(["pip", "install", "numpydoc"], check=True)
 
 
 # -- Project information -----------------------------------------------------
@@ -31,13 +39,52 @@ release = '2022'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.doctest",
+    "sphinx.ext.coverage",
+    "sphinx.ext.viewcode",
+    "numpydoc",
+    "sphinx_rtd_theme",
+    "sphinx.ext.intersphinx",
+    "sphinxarg.ext",
+    "nbsphinx",
+    "recommonmark",
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx_tabs.tabs',
+    'sphinx-prompt',
+    'sphinx_search.extension',
+    'sphinx.ext.intersphinx',
+    'sphinxemoji.sphinxemoji',
+    'myst_parser',
 ]
+
+nbsphinx_execute = "never"
+
+autosummary_generate = True
+autodoc_default_options = {
+    "members": True,
+    "inherited-members": True,
+    "member-order": "bysource",
+}
+
+numpydoc_show_class_members = True
+numpydoc_show_inherited_class_members = True
+numpydoc_attributes_as_param_list = True
+intersphinx_mapping = {
+    "pandas": ("https://pandas.pydata.org/docs", None),
+    "sklearn": ("https://scikit-learn.org/stable", None),
+    "numpy": ("https://numpy.org/doc/stable", None),
+    "matplotlib": ("https://matplotlib.org", None),
+}
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
 # The root document.
-root_doc = 'tigerfish_index.rst'
+root_doc = 'index.rst'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -50,7 +97,19 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+
+master_doc = "index"
+source_suffix = [".rst", ".md"]
+
+html_theme_options = {
+    "style_nav_header_background": "#343131",
+    "logo_only": True,
+}
+
+html_css_files = [
+    "custom.css",
+]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
