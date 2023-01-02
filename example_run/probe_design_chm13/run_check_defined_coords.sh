e5ff5ac1dd7a528238cbe53b47ec81d89a1fda0f
robinaguilar <eaguil@uw.edu>
2023-01-02 15:44:01 -0800

    updated paths to check

--- example_run/probe_design_chm13/run_check_defined_coords.sh
+++ example_run/probe_design_chm13/run_check_defined_coords.sh
@@ -1,2 +1,2 @@
 
-python ../../../../workflow/main/scripts/main/check_defined_coords.py -xt pipeline_output/03_output_files/01_final_probes/chr9_all_probes.tsv -xe expected_output/03_output_files/01_final_probes/chr9_all_probes.tsv
+python ../../workflow/scripts/check_defined_coords.py -xt pipeline_output/03_output_files/01_final_probes/chr9_all_probes.tsv -xe expected_output/03_output_files/01_final_probes/chr9_all_probes.tsv
